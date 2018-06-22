ddbccb69cce5ca289831a064b9376591fc58983a
Jamie Bull <jamie.bull@kiwi.com>
2018-06-22 20:29:07 +0200

    Recent shit-score

--- kw/skycrapper/blueprints/skycrapper.py
+++ kw/skycrapper/blueprints/skycrapper.py
@@ -1,5 +1,5 @@
-from datetime import datetime
-from ..settings import STALL_MAPPING
+from collections import defaultdict
+from datetime import datetime, timedelta
 
 from quart import Blueprint
 from quart import current_app
@@ -26,7 +26,7 @@ async def shit(stall):
     cur = current_app.db.cursor()
     cur.execute('''INSERT INTO shits VALUES (?, ?)''', (STALL_MAPPING[stall], datetime.utcnow()))
     db.commit()
-    return await make_response('Shit has been registered', 401)
+    return await make_response('Shit has been registered', 201)
 
 
 @skycrapper.route('/data', methods=['GET'])
@@ -35,9 +35,15 @@ async def test():
     Get the stats
     :return:
     """
-    # SQLITE DB
-    db = current_app.db
-    pass
+    cur = current_app.db.cursor()
+    cur.execute('''SELECT * FROM shits''')
+    data = cur.fetchall()
+    turdpile = defaultdict(float)
+    for stall, t_shit in data:
+        turd_age = (datetime.utcnow() - t_shit)
+        if turd_age < timedelta(hours=12):
+            turdpile[stall] += turd_age.hours ** -1
+    return await make_response(turdpile)
 
 
 @skycrapper.route('/show', methods=['GET'])
@@ -46,7 +52,6 @@ async def show():
     Dumps shits table for test
     :return:
     """
-    db = current_app.db
-    cur = db.cursor()
+    cur = current_app.db.cursor()
     cur.execute('''SELECT * FROM shits''')
     return await make_response(str(cur.fetchall()))
